8159677c545bc51e950b80a5f54c5b444c3debf3
jvalentine-pleme <41764900+jvalentine-pleme@users.noreply.github.com>
2018-09-05 09:29:41 -0500

    Add files via upload

---
+++ afterboot.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "This was an interesting challenge" >> /tmp/somefile
+echo "Cant say that it wasnt challenging" >> /tmp/somefile
+echo "Though, it only took me an hour" >> /tmp/somefile
+echo "I know the challenges will only get harder" >> /tmp/somefile
